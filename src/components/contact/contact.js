98a425029ffde7ea197df83db41eacc585d2ec7d
LindseyGunderson <65924727+LindseyGunderson@users.noreply.github.com>
2021-07-28 15:03:26 -0700

    Add contact section
    Added contact section with form and images.

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -3,6 +3,21 @@ import './contact.scss';
 export default function Contact() {
     return (
         <div className="contact" id="contact">
+
+            <div className="left">
+                <img src="assets/email-contact.svg" />
+            </div>
+
+            <div className="right">
+                <h2>Contact Me :)</h2>
+
+                <form>
+                    <div class="form-inputs"><img src="assets/account.png" /><input type="text" placeholder="Full Name"></input></div>
+                    <div class="form-inputs"><img src="assets/email.png" /><input type="text" placeholder="Email"></input></div>
+                    <div class="form-inputs"><img src="assets/message.png" /><textarea placeholder="Message"></textarea></div>
+                    <button>Send</button>
+                </form>
+            </div>
             
         </div>
     )
